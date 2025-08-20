27a810dd6ec71f5ce4bc3aed5fac043563aaff50
Joe McLaughlin <85720969+jmcglock@users.noreply.github.com>
2025-08-20 17:15:09 -0400

    Update update-allowlist-script.sh

--- update-allowlist-script.sh
+++ update-allowlist-script.sh
@@ -1,58 +1,58 @@
 #!/bin/bash
 
-API_KEY="YOUR_API_KEY_HERE"
-PROFILE_ID="YOUR_PROFILE_ID_HERE"
+API_KEY=""
+PROFILE_ID=""
 ENDPOINT="https://api.nextdns.io/profiles/${PROFILE_ID}/allowlist"
 
 domains=(
-    "apple.com"
-    "amp-api-edge.apps.apple.com"
-    "amp-api-search-edge.apps.apple.com"
-    "smoot.apple.com"
-    "microsoft.com"
-    "visualstudio.com"
-    "windows.com"
-    "windowsupdate.com"
-    "xboxlive.com"
-    "ghcr.io"
-    "github.com"
-    "github.io"
-    "gitlab.com"
-    "googleapis.com"
-    "discordapp.com"
-    "graph.facebook.com"
-    "graph.instagram.com"
-    "i.instagram.com"
-    "pinimg.com"
-    "soundcloud.com"
-    "spotify.com"
-    "scdn.co"
-    "s.youtube.com"
-    "ads-fa-darwin.hulustream.com"
-    "hulu.hb-api.omtrdc.net"
-    "megaphone.fm"
-    "nbcsports.com"
-    "peacocktv.com"
-    "dcf.espn.com"
-    "espncdn.com"
-    "gamepass.com"
-    "demdex.net"
-    "fonts.gstatic.com"
-    "intuit.com"
-    "nerdfonts.com"
-    "nextdns.io"
-    "omtrdc.net"
-    "rancher.io"
-    "simplelogin.io"
-    "typekit.net"
-    "ubuntu.com"
-    "ui.com"
-    "urbanairship.com"
-    "tesla.com"
-    "tesla.services"
-    "tessie.com"
-    "akamai.net"
-    "cloudflare.com"
+akamai.net
+ads-fa-darwin.hulustream.com
+amp-api-edge.apps.apple.com
+amp-api-search-edge.apps.apple.com
+apple.com
+cloudflare.com
+dcf.espn.com
+demdex.net
+discordapp.com
+espncdn.com
+fonts.gstatic.com
+gamepass.com
+ghcr.io
+github.com
+github.io
+gitlab.com
+googleapis.com
+graph.facebook.com
+graph.instagram.com
+hulu.hb-api.omtrdc.net
+i.instagram.com
+intuit.com
+megaphone.fm
+microsoft.com
+nbcsports.com
+nerdfonts.com
+nextdns.io
+omtrdc.net
+peacocktv.com
+pinimg.com
+rancher.io
+s.youtube.com
+scdn.co
+simplelogin.io
+smoot.apple.com
+soundcloud.com
+spotify.com
+tesla.com
+tesla.services
+tessie.com
+typekit.net
+ubuntu.com
+ui.com
+urbanairship.com
+visualstudio.com
+windows.com
+windowsupdate.com
+xboxlive.com
 )
 
 echo "Starting to add ${#domains[@]} domains..."
